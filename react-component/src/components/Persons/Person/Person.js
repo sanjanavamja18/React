f9eb8bb18756c2e0529f286008ab34c9ba7b1290
sanjanavamja18 <sanjana.vamja@exhibytesolution.com>
2022-07-06 17:53:32 +0530

    Life Cycle

--- react-component/src/components/Persons/Person/Person.js
+++ react-component/src/components/Persons/Person/Person.js
@@ -1,23 +1,9 @@
 import React from "react";
-import styled from "styled-components";
-//import "./Person.css";
-
-const StyledDiv = styled.div`
-  width: 60%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
-
-  @media (min-width: 500px) {
-    width: 450px;
-  }
-`;
+import "./Person.css";
 
 const person = (props) => {
   return (
-    <StyledDiv>
+    <div>
       <div className="Person">
         <p onClick={props.click}>
           I'm a {props.name} and I'm {props.age} years old!
@@ -25,7 +11,7 @@ const person = (props) => {
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name}></input>
       </div>
-    </StyledDiv>
+    </div>
   );
 };
 
